8b1c7ce4970ba56f135cb6b24e69b07b5e081b9a
frodriguez <frpatino6@gmail.com>
2021-04-29 21:13:04 -0500

    commit pendig files

--- Marshalls-LLC/src/Marshalls-LLC.Infrastructure/Migrations/20210430013440_Sp_Get_Last_Employee_Salary.Designer.cs
+++ Marshalls-LLC/src/Marshalls-LLC.Infrastructure/Migrations/20210430013440_Sp_Get_Last_Employee_Salary.Designer.cs
@@ -4,14 +4,16 @@ using Marshalls_LLC.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Marshalls_LLC.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210430013440_Sp_Get_Last_Employee_Salary")]
+    partial class Sp_Get_Last_Employee_Salary
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
